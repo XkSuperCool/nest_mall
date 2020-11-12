33abbcb18e2378eaad203f1bccbc69eeb9839cb1
= <=>
2020-11-12 18:03:04 +0800

    style: 修改图形验证码的默认配置

--- src/utils/captcha.ts
+++ src/utils/captcha.ts
@@ -2,6 +2,9 @@ import { create, ConfigObject, CaptchaObj } from 'svg-captcha';
 
 export default function createCaptcha(options?: ConfigObject): CaptchaObj {
   return create(Object.assign({
+    width: 90,
+    height: 32,
+    fontSize: 40,
     size: 4, // 验证码数量
     noise: 3, // 干扰线数量
     background: '#3786d6'
