d97ad14ce78462e3f08f08e42181b157ae573819
= <=>
2020-12-11 13:58:11 +0800

    feat:轮播组件

--- src/controller/api/tools.controller.ts
+++ src/controller/api/tools.controller.ts
@@ -1,12 +1,21 @@
 import { Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { saveFile } from '../../utils';
+import { join } from 'path';
+import { ErrorModel, SuccessModel } from '../../model/ResModel';
+import { uploadFileError } from 'src/model/errorInfo';
 
 @Controller('tools')
 export class ToolsController {
-
   @Post('upload')
   @UseInterceptors(FileInterceptor('file'))
-  uploadFile(@UploadedFile() file) {
-    console.log(file);
+  async uploadFile(@UploadedFile() file) {
+    try {
+      const src = join(__dirname, '..', '..', '..', 'assets');
+      const result = await saveFile(src, file);
+      return new SuccessModel(result);
+    } catch {
+      return new ErrorModel(uploadFileError.status, uploadFileError.msg);
+    }
   }
 }
